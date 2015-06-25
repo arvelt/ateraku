f0af6b37ff590418d8d0980e639218ac3dcbf126
Keisuke Ota <arvelt.s@gmail.com>
2015-06-25 12:15:06 +0900

    wip rest api

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+	url(r'^api/v1/events/(?P<id>\d+)', 'api.views.events'),
 	url(r'^api/v1/events', 'api.views.events_list'),
 	url(r'^api/v1/', 'api.views.home'),
     url(r'^$', 'api.hello.views.home'),
