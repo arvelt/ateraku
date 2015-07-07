725bd89261f312daf0718b8c5df5ef0aca40eccf
Keisuke Ota <arvelt.s@gmail.com>
2015-07-08 01:19:01 +0900

    improve data structure

--- api/forms.py
+++ api/forms.py
@@ -1,19 +1,59 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+class MultiIntegerField(forms.IntegerField):
+	# Need a widget for this type of multi-field
+	# (e.g. A widget of variable number of input fields)
+	hidden_widget = forms.widgets.MultipleHiddenInput
+	widget = forms.widgets.SelectMultiple
+
+	def to_python(self, value):
+		res = []
+		if isinstance(value, (list, tuple)):
+			for val in value:
+				val2 = super(MultiIntegerField, self).to_python(val)
+				res.append(val2)
+		return res
+
+
+class StructuredField(forms.Field):
+	def __init__(self, form_class, *args, **kwargs):
+		self.form_class = form_class
+		super(StructuredField, self).__init__(*args, **kwargs)
+
+	def to_python(self, value):
+		if value in ('', None):
+			return None
+		elif not isinstance(value, dict):
+			raise ValidationError()
+
+		form = self.form_class(value)
+		if form.is_valid():
+			return form.cleaned_data
+		else:
+			raise ValidationError()
+
 
 class EventForm(forms.Form):
 	name = forms.CharField()
-	organizer = forms.CharField()
+	organizer = forms.IntegerField()
 	description = forms.CharField(required=False)
 	place = forms.CharField(required=False)
 	date = forms.DateTimeField(required=False)
-	attend = forms.CharField(required=False)
-	interest = forms.CharField(required=False)
-	absence = forms.CharField(required=False)
+	attend = MultiIntegerField(required=False)
+	interest = MultiIntegerField(required=False)
+	absence = MultiIntegerField(required=False)
+
+
+class AuthForm(forms.Form):
+	user_id = forms.IntegerField(required=False)
+	token = forms.CharField(required=False)
+	secret = forms.CharField(required=False)
+
 
 class UserForm(forms.Form):
 	name = forms.CharField()
-	email = forms.CharField()
-	twitter_id = forms.CharField(required=False)
-	google_id = forms.CharField(required=False)
-	future_events = forms.CharField(required=False)
-	past_events = forms.CharField(required=False)
+	email = forms.CharField(required=False)
+	twitter = StructuredField(AuthForm, required=False)
+	future_events = MultiIntegerField(required=False)
+	past_events = MultiIntegerField(required=False)
--- api/models.py
+++ api/models.py
@@ -7,9 +7,9 @@ class Event(ndb.Model):
 	description = ndb.TextProperty()
 	place = ndb.StringProperty()
 	date = ndb.DateTimeProperty()
-	attend = ndb.StringProperty()
-	interest = ndb.StringProperty()
-	absence = ndb.StringProperty()
+	attend = ndb.KeyProperty(kind='User', repeated=True)
+	interest = ndb.KeyProperty(kind='User', repeated=True)
+	absence = ndb.KeyProperty(kind='User', repeated=True)
 
 	@classmethod
 	def get_by_pk(cls, id, **ctx_options):
@@ -18,17 +18,17 @@ class Event(ndb.Model):
 	@classmethod
 	def create(cls, **kwargs):
 		name = kwargs.get('name', '')
-		organizer = kwargs.get('organizer', '')
+		organizer = kwargs.get('organizer', None)
 		description = kwargs.get('description', '')
 		place = kwargs.get('place', '')
 		date = kwargs.get('date', None)
-		attend = kwargs.get('attend', None)
-		interest = kwargs.get('interest', None)
-		absence = kwargs.get('absence', None)
+		attend = kwargs.get('attend', [])
+		interest = kwargs.get('interest', [])
+		absence = kwargs.get('absence', [])
 
 		user = User.get_by_pk(organizer)
-
-		pprint(user)
+		if user is None:
+			raise ValueError()
 
 		event = Event(
 			name = name,
@@ -46,16 +46,38 @@ class Event(ndb.Model):
 	@classmethod
 	def update(cls, id, kwargs):
 		event = cls.get_by_pk(id)
+		organizer = User.get_by_pk(kwargs['organizer'])
+		if organizer is None:
+			raise ValueError()
+
+		attends = []
+		params_attends = kwargs.get('attend', [])
+		for user_pk in params_attends:
+			user = User.get_by_pk(user_pk)
+			attends.append(user.key)
+
+		interests = []
+		params_interests = kwargs.get('interest', [])
+		for user_pk in params_interests:
+			user = User.get_by_pk(user_pk)
+			interests.append(user.key)
+
+		absences = []
+		params_absences = kwargs.get('absence', [])
+		for user_pk in params_absences:
+			user = User.get_by_pk(user_pk)
+			absences.append(user.key)
+
 		if event:
 			event.populate(
 				name= kwargs['name'],
-				organizer= kwargs['organizer'],
+				organizer= organizer.key,
 				description= kwargs['description'],
 				place= kwargs['place'],
 				date= kwargs['date'],
-				attend= kwargs['attend'],
-				interest= kwargs['interest'],
-				absence= kwargs['absence']
+				attend= attends,
+				interest= interests,
+				absence= absences
 			)
 		event.put()
 		return event
@@ -66,26 +88,44 @@ class Event(ndb.Model):
 		return id
 
 	def to_dict(self):
+		attends = []
+		for user in self.attend:
+			attends.append(user.id())
+
+		interests = []
+		for user_pk in self.interest:
+			interests.append(user.id())
+
+		absences = []
+		for user_pk in self.absence:
+			absences.append(user.id())
+
 		dict = {
-			'id': str(self.key.id()),
+			'id': int(self.key.id()),
 			'name': self.name,
 			'organizer': self.organizer.id(),
 			'description': self.description,
 			'place': self.place,
-			'date': str(self.date),
-			'attend': str(self.attend),
-			'interest': str(self.interest),
-			'absence': str(self.absence),
+			'date': self.date,
+			'attend': attends,
+			'interest': interests,
+			'absence': absences,
 		}
 		return dict
 
+
+class AuthToken(ndb.Model):
+	user_id = ndb.IntegerProperty(required=False)
+	token = ndb.StringProperty(required=False)
+	secret = ndb.StringProperty(required=False)
+
+
 class User(ndb.Model):
 	name = ndb.StringProperty()
-	email = ndb.StringProperty()
-	twitter_id = ndb.StringProperty()
-	google_id = ndb.StringProperty()
-	future_events = ndb.KeyProperty()
-	past_events = ndb.KeyProperty()
+	email = ndb.StringProperty(required=False)
+	twitter = ndb.StructuredProperty(AuthToken, required=False)
+	future_events = ndb.KeyProperty(kind='Event', repeated=True, required=False)
+	past_events = ndb.KeyProperty(kind='Event', repeated=True, required=False)
 
 	@classmethod
 	def get_by_pk(cls, id, **ctx_options):
@@ -94,17 +134,31 @@ class User(ndb.Model):
 	@classmethod
 	def create(cls, **kwargs):
 		name = kwargs.get('name', '')
-		email = kwargs.get('organizer', '')
-		twitter_id = kwargs.get('description', '')
-		google_id = kwargs.get('place', '')
-		future_events = kwargs.get('date', None)
-		past_events = kwargs.get('attend', None)
+		email = kwargs.get('email', '')
+		twitter = kwargs.get('twitter', None)
+		if twitter is None:
+			twitter = {}
+
+		future_events = []
+		params_future_events = kwargs.get('future_events', [])
+		for event_pk in params_future_events:
+			event = Event.get_by_pk(event_pk)
+			future_events.append(event.key)
+
+		past_events = []
+		params_past_events = kwargs.get('past_events', [])
+		for event_pk in params_past_events:
+			event = Event.get_by_pk(event_pk)
+			past_events.append(event.key)
 
 		user = User(
 			name = name,
 			email = email,
-			twitter_id = twitter_id,
-			google_id = google_id,
+			twitter = AuthToken(
+				user_id = twitter.get('userId', 0),
+				token = twitter.get('token', ''),
+				secret = twitter.get('secret', ''),
+			),
 			future_events = future_events,
 			past_events = past_events,
 		)
@@ -115,13 +169,35 @@ class User(ndb.Model):
 	def update(cls, id, kwargs):
 		user = cls.get_by_pk(id)
 		if user:
+			twitter = kwargs.get('twitter', None)
+			if twitter is None:
+				twitter = {}
+			twitter_user_id = twitter.get('user_id', 0)
+			twitter_token = twitter.get('token', '')
+			twitter_secret = twitter.get('secret', '')
+
+			future_events = []
+			params_future_events = kwargs.get('future_events', [])
+			for event_pk in params_future_events:
+				event = Event.get_by_pk(event_pk)
+				future_events.append(event.key)
+
+			past_events = []
+			params_past_events = kwargs.get('past_events', [])
+			for event_pk in params_past_events:
+				event = Event.get_by_pk(event_pk)
+				past_events.append(event.key)
+
 			user.populate(
 				name= kwargs['name'],
 				email= kwargs['email'],
-				twitter_id= kwargs['twitter_id'],
-				google_id= kwargs['google_id'],
-				future_events= kwargs['future_events'],
-				past_events= kwargs['past_events'],
+				twitter= AuthToken(
+					user_id = twitter_user_id,
+					token = twitter_token,
+					secret = twitter_secret,
+				),
+				future_events= future_events,
+				past_events= past_events,
 			)
 		user.put()
 		return user
@@ -132,12 +208,23 @@ class User(ndb.Model):
 		return id
 
 	def to_dict(self):
+		future_events = []
+		for user in self.future_events:
+			future_events.append(user.id())
+
+		past_events = []
+		for user in self.past_events:
+			past_events.append(user.id())
+
 		return {
-			'id': str(self.key.id()),
+			'id': int(self.key.id()),
 			'name': self.name,
 			'email': self.email,
-			'twitter_id': self.twitter_id,
-			'google_id': self.google_id,
-			'future_users': str(self.future_events),
-			'past_events': str(self.past_events),
+			'twitter': {
+				'id': int(self.twitter.user_id),
+				'token': self.twitter.token,
+				'secret': self.twitter.secret,
+			},
+			'future_events': future_events,
+			'past_events': past_events,
 		}
